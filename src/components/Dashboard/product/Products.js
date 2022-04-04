8ce965dd3cd870fd896564a2ff910e3d436f905a
RiyadMia <96831477+RiyadMia@users.noreply.github.com>
2022-04-04 16:55:35 +0600

    somting file add

--- src/components/Dashboard/product/Products.js
+++ src/components/Dashboard/product/Products.js
@@ -1,5 +1,8 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
 import './Products.css'
+import Rating from 'react-rating';
 const Products = (props) => {
     const {name,img,price,rating,review,reviews}=props.product
     return (
@@ -9,7 +12,12 @@ const Products = (props) => {
      </div>  
       <h3>{name}</h3>
       <h2>price : $ {price}</h2>
-      <h2> Rating : {rating}</h2>
+      <Rating
+       initialRating={4.5}
+       emptySymbol={<FontAwesomeIcon icon={faStar} />}
+       fullSymbol={<FontAwesomeIcon style={{color: 'goldenrod'}} icon={faStar} />}
+       readonly
+      ></Rating>
       <h5>{reviews}</h5>
       <p>{review}</p>
      </div>
